78e55966c8f571817cef0f46647505e471b7a13b
ahmadsohailMask01 <ahmaddd.sohail759@gmail.com>
2024-02-03 19:59:30 +0500

    change

--- src/app/components/Products.js
+++ src/app/components/Products.js
@@ -1,13 +1,25 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import Productslist from "./Products_List";
 import styles from "../../../styles/products.module.css";
 import Link from "next/link";
 
 const Products = () => {
+  useEffect(() => {
+    const loading_func = () => {
+      document.getElementById("loading").style.display = "none";
+      document.getElementById("container").style.display = "flex";
+    };
+    setTimeout(loading_func, 2000);
+  }, []);
+
   return (
     <>
       <section className={styles.section_products}>
-        <div className={styles.products_container}>
+        <div className={styles.load} id="loading">
+          Loading...
+        </div>
+        <div className={styles.products_container} id="container">
           <Productslist />
         </div>
       </section>
